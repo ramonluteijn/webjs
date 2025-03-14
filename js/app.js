7b944fc2001d9cf3a6dd23e266179a0aebd6d77d
ramon luteijn <ramon@intractief.nl>
2025-03-14 10:35:33 +0100

    basic create / delete bucket

--- js/app.js
+++ js/app.js
@@ -1,8 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let div = document.getElementById("mainDiv");
-
-    div.innerHTML = "<div> Laat maar zitten </div>";
-
-
-    div.style.backgroundColor = "red";
+    addButton();
+    deleteButton();
 });
+
+function addButton() {
+    let button = document.createElement("button");
+    button.setAttribute("id", "addBucketButton");
+    button.innerHTML = "Add bucket";
+    button.onclick = function() {
+        addBucket();
+    }
+    document.body.appendChild(button);
+}
+
+function deleteButton() {
+    let button = document.createElement("button");
+    button.setAttribute("id", "deleteBucketButton");
+    button.innerHTML = "Delete bucket";
+    button.onclick = function() {
+        deleteBucket();
+    }
+    document.body.appendChild(button);
+}
+
+
+function addBucket() {
+    let div = document.getElementById("mainDiv");
+    let bucket = document.createElement("div");
+    bucket.setAttribute("class", "bucket");
+    bucket.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
+    bucket.style.width = "100px";
+    bucket.style.height = "100px";
+    div.appendChild(bucket);
+}
+
+function deleteBucket() {
+    let div = document.getElementById("mainDiv");
+    let bucket = document.getElementsByClassName("bucket");
+    for (let i = 0; i < bucket.length; i++) {
+        bucket[i].addEventListener("click", function() {
+            div.removeChild(bucket[i]);
+        });
+    }
+}
